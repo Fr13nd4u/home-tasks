14329ee4fdd00a1713dae8476d867636a3f240d4
mstoliar <mstoliar@softkitit.com>
2023-07-27 12:00:52 +0300

    remove note function

--- task_1/scripts/app.js
+++ task_1/scripts/app.js
@@ -1,8 +1,55 @@
-import { renderNotesTable } from './render.js';
+import { addNote, removeNote  } from './data.js';
+import { renderNotesTable, renderSummaryTable } from './render.js';
+
+const addNoteForm = document.querySelector('#add-note-form');
+const noteContentInput = document.querySelector('#note-content');
+const noteCategoryInput = document.querySelector('#note-category');
+
+// Handle "Add Note" event
+addNoteForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const noteContent = noteContentInput.value.trim();
+  const noteCategory = noteCategoryInput.value;
+
+  if (!noteContent || !noteCategory) {
+    alert('Please fill in both Note Content and Note Category.');
+    return;
+  }
+
+  const newNote = {
+    id: Date.now(),
+    time: new Date().toISOString(),
+    content: noteContent,
+    category: noteCategory,
+    dates: [], 
+    archived: false,
+  };
+
+  addNote(newNote);
+
+  noteContentInput.value = '';
+  noteCategoryInput.value = '';
+
+  init()
+});
+
+// Handle "Remove" button click event
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('remove')) {
+    const noteId = parseInt(event.target.dataset.noteId);
+
+    removeNote(noteId);
+
+    init()
+  }
+});
 
 function init() {
   renderNotesTable();
+  renderSummaryTable();
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
 
--- task_1/scripts/render.js
+++ task_1/scripts/render.js
@@ -1,22 +1,45 @@
-import { getNotes } from './data.js';
+import { getNotes, countNotesByCategory } from './data.js';
 
 export function renderNotesTable() {
   const notesTable = document.querySelector('#notes-table tbody');
   const notes = getNotes();
 
-  // Clear previous content
   notesTable.innerHTML = '';
 
   // Render new notes rows
   notes.forEach((note) => {
     const row = notesTable.insertRow();
     row.innerHTML = `<td>${note.time}</td><td>${note.content}</td><td>${note.category}</td><td>${note.dates.join(', ')}</td><td>
-      <button onclick="editNoteHandler(${note.id})">Edit</button>
-      <button onclick="removeNoteHandler(${note.id})">Remove</button>
-      <button onclick="archiveNoteHandler(${note.id})">Archive</button>
-      <button onclick="unarchiveNoteHandler(${note.id})">Unarchive</button>
+      <button class="edit" data-note-id="${note.id}">Edit</button>
+      <button class="remove" data-note-id="${note.id}">Remove</button>
+      <button class="archive" data-note-id="${note.id}">Archive</button>
+      <button class="unarchive" data-note-id="${note.id}">Unarchive</button>
     </td>`;
   });
 
-  notesTable.appendChild(table);
 }
+
+
+function renderSummaryTable() {
+  const summaryTable = document.getElementById('summary-table');
+  summaryTable.innerHTML = '';
+
+  const table = document.createElement('table');
+  const headerRow = table.insertRow();
+  headerRow.innerHTML = '<th>Category</th><th>Count</th>';
+
+  const summary = countNotesByCategory();
+  Object.keys(summary).forEach((category) => {
+    const row = table.insertRow();
+    row.innerHTML = `<td>${category}</td><td>${summary[category]}</td>`;
+  });
+
+  summaryTable.appendChild(table);
+}
+
+function updateTables() {
+  renderNotesTable();
+  renderSummaryTable();
+}
+
+export { updateTables, renderSummaryTable };
